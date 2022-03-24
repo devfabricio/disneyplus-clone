f8cda6737548fa0d172017500de92f06f5e29719
Fabrício Augusto <fabricio.mata@gmail.com>
2022-03-24 14:12:45 -0300

    fetch content from data.json

---
+++ project/assets/js/components/control-slider-item.js
@@ -0,0 +1,7 @@
+const ControlSliderItem = () => {
+    return `
+        <button class="controls-slider__item" data-banner="btn-control"></button>
+    `
+}
+
+export default ControlSliderItem
--- project/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import header from './modules/header.js'
-import bannerSlider from './modules/banner-slider.js'
-import collections from './modules/collections.js'
-
-header.init()
-bannerSlider.init()
-collections.init()
