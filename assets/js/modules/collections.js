29765e1f23ae31f51e273c1b6440121e3a2ad481
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-08-02 20:23:28 -0300

    carousel

--- assets/js/modules/collections.js
+++ assets/js/modules/collections.js
@@ -1,38 +1,38 @@
-const carouselList =document.querySelector('[data-carousel="list"');
-const carouselItems = document.querySelectorAll('[data-carousel="item"]')
-const btnPrevious = document.querySelector('[data-carousel="btn-previous"]')
-const btnNext = document.querySelector('[data-carousel="btn-next"]')
-
-
-const state ={
-    mouseDownPosition: 0,
-    movement: 0,
-    lastTranslatePosition: 0,
-    currentSlidePosition: 0,
-    currentItemIndex: 0,
-    currentSlideIndex: 0,
-}
+const collections = document.querySelectorAll('[data-carousel="collection"]')
+const collectionData = []
+let currentCollectionIndex = 0
+let itemsPerSLide = 5
 
 const preventDefault = (event) => {
     event.preventDefault()
 }
 
 const translateSlide = (position) => {
+
+    const {state, carouselList } = collectionData[currentCollectionIndex]
+
     state.lastTranslatePosition = position
     carouselList.style.transform = `translateX(${position}px)`
 }
 
 const getCenterPosition = (slideIndex) => {
+    const {state, carouselItems } = collectionData[currentCollectionIndex]
     const item = carouselItems[state.currentItemIndex]
     const itemWidth = item.offsetWidth
     const bodyWidth = document.body.clientWidth
-    const slideWidth = itemWidth * 5
+    const slideWidth = itemWidth * itemsPerSLide
     const margin = (bodyWidth - slideWidth) / 2
-    
     return margin - (slideWidth * slideIndex)
 }
 
+const getLastSlideIndex = () => {
+    const { carouselItems } = collectionData[currentCollectionIndex]
+    const lasItemIndex = carouselItems.length -1
+    return Math.floor(lasItemIndex / itemsPerSLide)
+}
+
 const animateTransistion = (active) => {
+    const { carouselList } = collectionData[currentCollectionIndex]
     if(active) {
         carouselList.style.transition = "transform .3s"
     } else {
@@ -41,6 +41,7 @@ const animateTransistion = (active) => {
 }
 
 const setVisibleSlide = (slideIndex) => {
+    const { state } = collectionData[currentCollectionIndex]
     state.currentSlideIndex = slideIndex
     const centerPosition = getCenterPosition(slideIndex)
     animateTransistion(true)
@@ -49,6 +50,7 @@ const setVisibleSlide = (slideIndex) => {
 }
   
 const backwardSlide = () => {
+    const { state } = collectionData[currentCollectionIndex]
     if(state.currentSlideIndex > 0) {
         setVisibleSlide(state.currentSlideIndex - 1)
     } else {
@@ -57,8 +59,8 @@ const backwardSlide = () => {
 }
 
 const forwardSlide = () => {
-    const lasItemIndex = carouselItems.length -1
-    const lastSlideIndex = Math.floor(lasItemIndex / 5)
+    const {state} = collectionData[currentCollectionIndex]
+    const lastSlideIndex = getLastSlideIndex
     if(state.currentSlideIndex < lastSlideIndex) {
         setVisibleSlide(state.currentSlideIndex + 1)
     } else {
@@ -67,22 +69,24 @@ const forwardSlide = () => {
     }    
 }
 
-const onMouseDown = (event, index) => {
+const onMouseDown = (event, itemIndex) => {
+    const { state } = collectionData[currentCollectionIndex]
     const item = event.currentTarget
-    state.currentItemIndex = index
+    state.currentItemIndex = itemIndex
     state.mouseDownPosition = event.clientX
     state.currentSlidePosition = event.clientX - state.lastTranslatePosition
     animateTransistion(false)
     item.addEventListener('mousemove', onMouseMove)
 }
 const onMouseMove = (event) => {
+    const { state } = collectionData[currentCollectionIndex]
     state.movement = event.clientX - state.mouseDownPosition
     const position = event.clientX - state.currentSlidePosition
     translateSlide(position)
     
 }
 const onMouseUp = (event) => {
-
+    const { state } = collectionData[currentCollectionIndex]
     if(state.movement > 150) {
         backwardSlide()
     } else if (state.movement < -150) {
@@ -90,7 +94,6 @@ const onMouseUp = (event) => {
     } else {
         setVisibleSlide(state.currentSlideIndex)
     }
-
     let item = event.currentTarget
     item.removeEventListener('mousemove', onMouseMove)
     
@@ -101,26 +104,102 @@ const onMouseLeave = (event) => {
     
 }
 
-const setListenners = () => {
-    btnNext.addEventListener('click', forwardSlide)
-    btnPrevious.addEventListener('click', backwardSlide)
-    carouselItems.forEach((item, index) => {
-        let link = item.querySelector('.movie-carousel__link')
+const onTouchStart = (event, itemIndex) => {
+    const item = event.currentTarget
+    item.addEventListener('touchmove', onTouchMove)
+    event.clientX = event.touches[0].clientX
+    onMouseDown(event, itemIndex)
+}
+
+const onTouchMove = (event) =>{
+    event.clientX = event.touches[0].clientX
+    onMouseMove(event)
+}
+
+const onTouchEnd = (event) =>{
+    const item = event.currentTarget
+    item.removeEventListener('touchmove', onTouchMove)
+    onMouseUp(event)
+}
+
+const insertCollectionData = (collection) => {
+    collectionData.push({
+        carouselList: collection.querySelector('[data-carousel="list"'),
+        carouselItems: collection.querySelectorAll('[data-carousel="item"]'),
+        btnPrevious: collection.querySelector('[data-carousel="btn-previous"]'),
+        btnNext: collection.querySelector('[data-carousel="btn-next"]'),
+        state: {
+            mouseDownPosition: 0,
+            movement: 0,
+            lastTranslatePosition: 0,
+            currentSlidePosition: 0,
+            currentItemIndex: 0,
+            currentSlideIndex: 0
+        }
+    })
+}
+
+const setItemsPerSlide = () => {
+    if(document.body.clientWidth < 1125){
+        itemsPerSLide = 2
+        return
+    }
+    itemsPerSLide = 5
+}
+const setWindowResizeListerner = () => {
+    let resizeTimeOut;
+    window.addEventListener('resize',(event) => {
+        clearTimeout(resizeTimeOut)
+        resizeTimeOut = setTimeout(() =>{
+            setItemsPerSlide()
+            collections.forEach((_, collectionIndex) => {
+                currentCollectionIndex = collectionIndex
+                setVisibleSlide(0)
+            })
+        }, 1000)
+    })
+}
+
+const setListenners = (collectionIndex) => {
+    const { btnNext, btnPrevious, carouselItems } = collectionData[collectionIndex]
+    btnNext.addEventListener('click', () => {
+        currentCollectionIndex = collectionIndex
+        forwardSlide()
+    })
+    btnPrevious.addEventListener('click', () => {
+        currentCollectionIndex = collectionIndex
+        backwardSlide()
+    })
+    carouselItems.forEach((item, itemIndex) => {
+        const link = item.querySelector('.movie-carousel__link')
         link.addEventListener('click', preventDefault)
         item.addEventListener('dragstart', preventDefault)
         item.addEventListener('mousedown', (event) =>{
-            onMouseDown(event, index)
+            currentCollectionIndex = collectionIndex
+            onMouseDown(event, itemIndex)
         } )
         item.addEventListener('mouseup', onMouseUp)
         item.addEventListener('mouseleave', onMouseLeave)
+        item.addEventListener('touchstart', (event) => {
+            currentCollectionIndex = collectionIndex
+            onTouchStart(event, itemIndex)
+        } )
+        item.addEventListener('touchend', onTouchEnd )
 
 
     })
 }
 
 const init = () => {
-    setListenners()
-    setVisibleSlide(0)
+    setItemsPerSlide()
+    setWindowResizeListerner()
+    collections.forEach((collection, collectionIndex) => {
+        currentCollectionIndex = collectionIndex
+        insertCollectionData(collection)
+        setListenners(collectionIndex)
+        setVisibleSlide(0)
+        console.log(insertCollectionData);
+    })
 }
 
 
