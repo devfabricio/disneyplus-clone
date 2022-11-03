6c54420c21f89776a0de7eaa1250944f0fbaaec0
vagnerb79 <spotify.vagner@gmail.com>
2022-11-03 16:52:51 -0300

    Finalizando header

---
+++ assets/js/modules/header.js
@@ -0,0 +1,40 @@
+const header = document.querySelector('[data-header]')
+const openNavSubmenu = document.querySelector ('[data-open-navsubmenu]')
+const navSubmenu = document.querySelector ('[data-navsubmenu]')
+const openUserMenu =document.querySelector ('[data-open-usermenu')
+const userMenu =document.querySelector ('[data-usermenu')
+
+
+
+function onWindowScroll (){
+    if(window.scrollY >20){
+        header.style.backgroundColor ='#0C0D14'
+    } else{
+        header.style.backgroundColor ='transparent'
+    }
+}
+
+function onTouchOpenNavSubmenu(event){
+    event.preventDefault()
+    navSubmenu.classList.toggle('active')
+}
+
+function onTouchOpenUserMenu(event){
+    event.preventDefault()
+    userMenu.classList.toggle('active')
+}
+
+
+function setListeners(){
+    window.addEventListener ('scroll', onWindowScroll)
+    openNavSubmenu.addEventListener ("touchstart", onTouchOpenNavSubmenu)
+    openUserMenu.addEventListener ("touchstart", onTouchOpenUserMenu)
+}
+
+function init(){
+    setListeners()
+}
+
+export default {
+    init
+}
