68e758c16477f47cb5338f9d02e616dd3bbd08c2
Higor Rodrigues <higor_1992@hotmail.com>
2022-11-24 22:40:21 -0300

    add behavior on carousel with javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 import header from './modules/header.js'
 import bannerSlider from './modules/banner-slider.js'
+import collections from './modules/collections.js'
 
 header.init()
 bannerSlider.init()
+collections.init()
